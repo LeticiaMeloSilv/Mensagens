bc00cad83528535f5d14ba175611a4839d320415
LeticiaMeloSilv <123740554+LeticiaMeloSilv@users.noreply.github.com>
2023-09-15 17:09:55 -0300

    Add files via upload

--- Cards/app.js
+++ Cards/app.js
@@ -4,6 +4,7 @@ const funcionarios=[
     destinatario: 'Jerome Bueno',
     foto_destinatario:'jerome_bueno.jpg',
     mensagem: 'aaa',
+    notificacao: '1',
     tempo:'Há 30 minutos',
     remetente: 'João Paulo'
   },
@@ -11,6 +12,7 @@ const funcionarios=[
     destinatario: 'Daniel Silva',
     foto_destinatario:'daniel_silva.jpg',
     mensagem: 'bbb',
+    notificacao: '3',
     tempo:'Há 30 minutos',
     remetente: 'Márcia'
   }, 
@@ -18,6 +20,7 @@ const funcionarios=[
     destinatario: 'Bruno Adorno',
     foto_destinatario:'bruno_adorno.jpg',
     mensagem: 'ccc',
+    notificacao: '9',
     tempo:'Há 30 minutos',
     remetente: 'Joysi'
   }  
@@ -34,21 +37,27 @@ function criarCards(funcionarios){
     imagem.alt = funcionarios.destinatario//laranja
 
     const divContainer = document.createElement('div')//amarelo
+    divContainer.classList.add('divContainer')//amarelo
 
     const destinatario = document.createElement('h3')//verde
     destinatario.textContent = funcionarios.destinatario//verde
+    destinatario.classList.add('destinatario')//vermelho
 
     const notificacao = document.createElement('span')//vinho
     notificacao.textContent = funcionarios.notificacao//vinho
+    notificacao.classList.add('notificacao')//vermelho
     
     const tempo = document.createElement('h5')//azul claro
     tempo.textContent = funcionarios.tempo//azul claro
+    tempo.classList.add('tempo')//vermelho
     
     const mensagem = document.createElement('p')//azul escuro
     mensagem.textContent = funcionarios.mensagem//azul escuro
     
     const remetente = document.createElement('h3')//roxo
     remetente.textContent = funcionarios.remetente//roxo
+    remetente.classList.add('remetente')//roxo
+
 
     divContainer.replaceChildren(destinatario, notificacao, tempo, mensagem, remetente)
 
